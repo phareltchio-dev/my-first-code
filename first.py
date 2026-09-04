c954a38157cb28b13f0d20bc163d04075ef5dcd3
Pharel Tchio <phareltchio08@gmail.com>
2026-09-04 20:43:49 +0100

    Add first Python program with personal introduction

---
+++ first.py
@@ -0,0 +1,8 @@
+# My first Python Program
+name = "Pharel Tchio"
+gce_results = "11 A Grades"
+
+print("Hello World!")
+print("My name is " + name)
+print("I scored " + gce_results + " in the GCE Ordinary Level!")
+print("Future Software Engineer loading...")
